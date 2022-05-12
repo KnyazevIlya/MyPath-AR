e87679b4dd70768902ec21086cdc8a2db2be0984
knyazevilya <kknyaz033@gmail.com>
2022-05-12 20:16:05 +0300

    Initial Commit

---
+++ MyPath/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPath
+//
+//  Created by Illia Kniaziev on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
